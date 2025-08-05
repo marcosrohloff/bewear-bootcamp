68000197c07def132b18806d9dc72da75b5c723d
Marcos Fernando <marcos.fersh@gmail.com>
2025-08-04 23:11:09 -0300

    alteracao do tema padrao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
-    return (
-        <div>
-            <Button>Bootcamp</Button>
-        </div>
-    );
+  return (
+    <div>
+      <Button>Bootcamp</Button>
+    </div>
+  );
 };
 
 export default Home;
