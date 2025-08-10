6f4df35e9c2f4d7d98af9c75f179baa522d6aecc
Marcos Fernando <marcos.fersh@gmail.com>
2025-08-10 17:49:19 -0300

    criacao das tabelas  cartTable e cartItemTable

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -15,6 +15,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
+import { Cart } from "./cart";
 
 export const Header = () => {
   const { data: session } = authClient.useSession();
@@ -23,7 +24,8 @@ export const Header = () => {
       <Link href="/">
         <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
       </Link>
-      <div className="flex items-center">
+
+      <div className="flex items-center gap-3">
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
@@ -78,6 +80,7 @@ export const Header = () => {
             </div>
           </SheetContent>
         </Sheet>
+        <Cart />
       </div>
     </header>
   );
